2fb3833e745d83213ee11f9dad9fbfa6e5acb90b
Emelie Quach <emeliele567@hotmail.se>
2025-03-10 22:20:55 +0100

    add settings page and implement lightmode/darkmode

--- backend/src/main/resources/templates/js/site.js
+++ backend/src/main/resources/templates/js/site.js
@@ -1,3 +1,4 @@
+//--    SIDE NAVIGATION   --
 function toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.classList.toggle('collapsed');
@@ -13,4 +14,34 @@ document.addEventListener('DOMContentLoaded', () => {
   if (isCollapsed) {
       document.querySelector('.sidebar').classList.add('collapsed');
   }
+});
+
+
+//--    CHANGE THEME    --
+const html = document.documentElement;
+const ThemeSelector = document.getElementById('themeSelector');
+const themeButtons = document.querySelectorAll('.themeButton');
+
+// Load theme from localStorage or default to 'light' if not set.
+const savedTheme = localStorage.getItem('theme') || 'light';
+ThemeSelector.value = savedTheme;
+applyTheme(savedTheme);
+
+function applyTheme(theme) {
+  html.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+
+  themeButtons.forEach(button => {
+    if (theme === 'dark') {
+        button.classList.remove('btn-outline-dark');
+        button.classList.add('btn-outline-light');
+    } else {
+        button.classList.remove('btn-outline-light');
+        button.classList.add('btn-outline-dark');
+    }
+  });
+}
+
+ThemeSelector.addEventListener('change', function() {
+  applyTheme(this.value);
 });
