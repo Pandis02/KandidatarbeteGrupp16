a8b511707bc9dac279d58e9dcbe1c1ff54aaef36
Emelie Quach <emeliele567@hotmail.se>
2025-04-01 14:10:28 +0200

    Add line chart

--- backend/src/main/resources/static/js/myCharts.js
+++ backend/src/main/resources/static/js/myCharts.js
@@ -13,22 +13,48 @@ new Chart("alertChart", {
     }]
   },
   options: {
-    plugins:{
-        legend: {display: false},
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "Number of alerts per week",
+        font: { weight: 'bold' }
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
         title: {
           display: true,
-          text: "Number of alerts per week",
-          font: {weight: 'bold'}
         },
-        scales:{
-            y:{
-                beginAtZero: true,
-                title:{
-                    display: true,
-                    text: 'number of alerts'
-                }
-            }
+        ticks: {
+          stepSize: 1 
         }
+      }
     }
   }
+});
+
+
+
+/* LINE CHART */
+var xTime = ["00:00", "04:00", "08:00", "12:00", "16:00", "20:00", "23:59"];
+
+const ctx = document.getElementById('notificationChart').getContext('2d');
+const notificationChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: xTime, 
+        datasets: [{
+            label: 'Number of Notifications',
+            data: [5, 10, 15, 8, 12, 18, 7], 
+            borderColor: 'blue',
+            fill: false,
+            tension: 0.3
+        }]
+    },
+    options: {
+        responsive: true,
+    }
 });
