168f26525d8dad3f6b3602baf29945fe4f884a00
hxkhan <mail@hxkhan.dev>
2025-04-03 01:39:40 +0200

    missed this

--- backend/src/main/java/kg16/demo/web/MainController.java
+++ backend/src/main/java/kg16/demo/web/MainController.java
@@ -1,14 +1,15 @@
 package kg16.demo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class MainController {
     public MainController() {
     }
 
     @GetMapping("/")
     public String onIndex() {
-        return "Hello World";
+        return "redirect:/dashboard";
     }
 }
