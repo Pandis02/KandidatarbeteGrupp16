8fad32698b59e6dd30b94bf81eed58791ad42f62
Emelie Quach <emelieq@student.chalmers.se>
2025-04-04 03:16:20 +0200

    change color of line chart

--- backend/src/main/resources/static/js/myCharts.js
+++ backend/src/main/resources/static/js/myCharts.js
@@ -1,13 +1,13 @@
 var xValues = ["Mon", "Tue", "Wed", "Thu", "Sat", "Sun"];
 var yValues = [1,0, 2, 1, 0, 5];
-var color = 'rgba(54, 87, 235, 0.47)';
+var purple = 'rgba(54, 87, 235, 0.47)';
 
 new Chart("alertChart", {
   type: "bar",
   data: {
     labels: xValues,
     datasets: [{
-      backgroundColor: color,
+      backgroundColor: purple,
       data: yValues,
       borderRadius: 5
     }]
@@ -39,6 +39,7 @@ new Chart("alertChart", {
 
 /* LINE CHART */
 var xTime = ["00:00", "04:00", "08:00", "12:00", "16:00", "20:00", "23:59"];
+var red = 'rgba(220,53,69,255)';
 
 const ctx = document.getElementById('notificationChart').getContext('2d');
 const notificationChart = new Chart(ctx, {
@@ -46,14 +47,21 @@ const notificationChart = new Chart(ctx, {
     data: {
         labels: xTime, 
         datasets: [{
-            label: 'Number of Notifications',
             data: [5, 10, 15, 8, 12, 18, 7], 
-            borderColor: 'blue',
+            borderColor: red,
             fill: false,
             tension: 0.3
         }]
     },
     options: {
         responsive: true,
+        plugins: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: "Alert frequency by time of day",
+            font: { weight: 'bold' }
+          }
+        },
     }
 });
